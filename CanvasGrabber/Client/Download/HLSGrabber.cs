00011257b7e7cc489ce460440fe9b7bb851da95a
Pieterjan <pielambr.Lambrecht@UGent.be>
2014-06-20 16:34:43 +0200

    Init

---
+++ CanvasGrabber/Client/Download/HLSGrabber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanvasGrabber.Client.Download
+{
+    class HLSGrabber
+    {
+        private Uri _grabberManifestUri;
+
+        public Uri GrabberManifestUri
+        {
+            get { return _grabberManifestUri; }
+            set { _grabberManifestUri = value; }
+        }
+
+        public async void start()
+        {
+
+        }
+
+        public async Task<bool> searchManifest(string uri)
+        {
+            return false;
+        }
+
+        public void setManifest(string uri)
+        {
+            GrabberManifestUri = new Uri(uri);
+        }
+
+
+
+    }
+}
---
+++ CanvasGrabber/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CanvasGrabber.Client.Download;
+
+namespace CanvasGrabber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HLSGrabber _grabber;
+
+        public MainWindow()
+        {
+            _grabber = new HLSGrabber();
+            InitializeComponent();
+        }
+
+        private async void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            if (chkManifest.IsChecked.Value)
+            {
+                if (!String.IsNullOrEmpty(txtManifestUri.Text))
+                {
+                    txtProgress.Text = "Starting grabber...";
+                    _grabber.setManifest(txtManifestUri.Text);
+                    _grabber.start();
+                }
+                else
+                {
+                    txtProgress.Text = "No manifest Uri provided...";
+                    progressDownload.Visibility = Visibility.Hidden;
+                }
+            }
+            else
+            {
+                if(!String.IsNullOrEmpty(txtWebsiteUri.Text)){
+                    txtProgress.Text = "Searching for manifest file...";
+                    progressDownload.Visibility = Visibility.Visible;
+                    bool found = await _grabber.searchManifest(txtWebsiteUri.Text);
+                    if(!found){
+                        progressDownload.Visibility = Visibility.Hidden;
+                        txtProgress.Text = "Manifest not found...";
+                    } else {
+                        txtProgress.Text = "Starting grabber...";
+                        _grabber.start();
+                    }
+                }
+                else
+                {
+                    txtProgress.Text = "No website Uri provided...";
+                    progressDownload.Visibility = Visibility.Hidden;
+                }
+                
+            }
+        }
+
+
+    }
+}
